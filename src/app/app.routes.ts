978dca060ca006dfbbc271d66453e379722a1d40
Kvantym <144225547+Kvantym@users.noreply.github.com>
2026-01-15 23:29:40 +0200

    Додав привітання

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,23 +6,27 @@ import { CreateBoardComponent } from './board/createBoard/createboard.component'
 import { ShowBoardComponent } from './board/showboard/showboard.component';
 import { UpdateUserComponent } from './auth/profil/profil.component';
 import {MainLayoutComponent} from './main-layout-component/main-layout-component';
+import {WelcomeComponent} from './welcome-component/welcome-component';
 
 export const routes: Routes = [
+  // СТОРІНКИ БЕЗ НАВІГАЦІЇ "ГОЛОВНА"
+  { path: '', component: WelcomeComponent }, // Твоя нова головна сторінка
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+
 
   {
     path: '',
     component: MainLayoutComponent,
     children: [
+      { path: 'dashboard', component: DashboardComponent },
       { path: 'create-board', component: CreateBoardComponent },
       { path: 'boards/:id', component: ShowBoardComponent },
       { path: 'profil', component: UpdateUserComponent },
     ]
   },
 
-  { path: '**', redirectTo: 'login' },
+  { path: '**', redirectTo: '' }, // Якщо помилка — на головну
 ];
 
 

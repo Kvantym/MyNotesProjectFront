8d673e12e597348bc30178bd2791aac08dd64bf7
Kvantym <144225547+Kvantym@users.noreply.github.com>
2026-01-15 23:16:17 +0200

    Додав на голвону

---
+++ src/app/main-layout-component/main-layout-component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout-component',
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
+  templateUrl: './main-layout-component.html',
+  styleUrl: './main-layout-component.scss'
+})
+export class MainLayoutComponent {
+
+}

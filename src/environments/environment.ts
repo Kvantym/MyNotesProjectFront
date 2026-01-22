8849609b8165f8f5bd52ba52ce1645a5ef4120a0
Kvantym <144225547+Kvantym@users.noreply.github.com>
2026-01-22 21:12:07 +0200

    Фікс багів із юай(половина отктата до норми)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tabledashboard-hkg4abg6hzgxd0hu.westeurope-01.azurewebsites.net/api' 
+  //apiUrl: 'https://tabledashboard-hkg4abg6hzgxd0hu.westeurope-01.azurewebsites.net/api'
+  apiUrl: 'http://localhost:5000/api'
+
 };
